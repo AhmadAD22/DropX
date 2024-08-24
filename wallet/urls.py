62f9400529d1a8682fe23b1178af83be3890b795
AhmadAD22 <ahmadadrah0@gmail.com>
2024-08-24 05:49:02 +0300

    payment v2

--- wallet/urls.py
+++ wallet/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.e_payment.common import *
 urlpatterns = [
-    path('e_payment',PaymentCallbackView.as_view()),
+    path('e_payment/callback',PaymentCallbackView.as_view()),
     path('finalize',finalizePayment,name='finalize_payment'),
 ]
